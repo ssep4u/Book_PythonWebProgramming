eece439d8edb447383d3f829e5d4815215972c0c
SSep <ssep4u@gmail.com>
2018-08-24 21:41:27 +0900

    fix

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -6,7 +6,7 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'modify_date',)
     list_filter = ('modify_date',)
     search_fields = ('title', 'content',)
-    prepopuplated_fields = {'slug':('title', )}
+    prepopulated_fields = {'slug':('title', )}
     
 
 
