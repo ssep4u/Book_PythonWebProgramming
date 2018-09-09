84712b534299e0dfd8343e0266e3d4f57ef90aaa
SSep <ssep4u@gmail.com>
2018-09-09 11:50:19 +0900

    tagging views, templates

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView
 from django.views.generic.dates import ArchiveIndexView, YearArchiveView, MonthArchiveView, DayArchiveView, TodayArchiveView
 from .models import Post
 from tagging.models import Tag, TaggedItem
