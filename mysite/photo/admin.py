bf264a79b7feab7c9c821939ac857934d02db3ed
SSep <ssep4u@gmail.com>
2018-09-09 23:41:37 +0900

    photo models

--- mysite/photo/admin.py
+++ mysite/photo/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Album, Photo
 
-# Register your models here.
+class PhotoInline(admin.StackedInline):
+    model = Photo
+    extra = 2
+
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    inlines = [PhotoInline]
+    list_display = ('name', 'description')
+
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'upload_date')
--- mysite/photo/models.py
+++ mysite/photo/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from .fields import ThumbnailImageField
 
-# Create your models here.
+
+class Album(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField('One Line Description', max_length=100, blank = True)
+    
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("photo:album_detail", args=(self.id,))
+
+
+class Photo(models.Model):
+    album = models.ForeignKey(Album)
+    title = models.CharField(max_length=50)
+    image = ThumbnailImageField(upload_to='photo/%Y/%m')
+    description = models.TextField('Photo Description', blank = True)
+    upload_date = models.DateTimeField('Upload Date', auto_now=False, auto_now_add=True)
+    
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("photo:Photo_detail", args=(self.id,))
